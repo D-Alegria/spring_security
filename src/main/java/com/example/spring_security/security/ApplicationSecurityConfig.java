ca5f65124ae20e280e667f2f6d054907647826da
Oladugba Demilade <demiladeoladugba@gmail.com>
2022-02-24 19:52:52 +0100

    5 - IN MEMORY USER DETAILS MANAGER

--- src/main/java/com/example/spring_security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/spring_security/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.spring_security.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Created by Demilade Oladugba on 2/24/2022
@@ -12,6 +17,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("demilade")
+                .password("password")
+                .roles("STUDENT")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
